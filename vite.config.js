50e4ec578db461bd2c9fb1e301ca63d803bda55f
cyobi164 <mrgnchl090@gmail.com>
2025-02-06 14:43:48 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/info/', // Keep this if needed for deployment
-  build: {
-    sourcemap: true, // Ensure source maps are generated
-  },
+  base: '/info/', // This must match your repo name!
 });
