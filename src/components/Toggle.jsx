6c4635c8e81bf9e4d634a6c9742241a490cd03b5
cyobi164 <mrgnchl090@gmail.com>
2025-02-23 02:24:52 +0900

    sleep

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './Toggle.css';
+import { useState, useEffect } from 'react';
+
 
 export const Toggle = ({ handleChange, isChecked }) => {
+
+    const [isHidden, setIsHidden] = useState(false);
+  
+    useEffect(() => {
+      let lastScrollY = window.scrollY;
+  
+      const handleScroll = () => {
+        if (window.scrollY > lastScrollY) {
+          // User is scrolling down, hide the header
+          setIsHidden(true);
+        } else {
+          // User is scrolling up, show the header
+          setIsHidden(false);
+        }
+        lastScrollY = window.scrollY > 0 ? window.scrollY : 0;
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+  
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
   return (
-    <div className="toggle-container">
+    <div className={`toggle-container ${isHidden ? 'hidden' : ''}`}>
       <input
         type="checkbox"
         id="check"
